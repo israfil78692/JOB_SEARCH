86c2e34f4335a0c8a91e6189910ecb086d43f6b5
md israfil ansari <ansariisrafil03@gmail.com>
2025-03-11 02:19:39 +0530

    deployed

--- Backend/index.js
+++ Backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 const corsOption={
-    origin:'http://localhost:5173',
+    origin:'https://job-search-gkml.onrender.com',
     credentials:true
 }
 app.use(cors(corsOption))
